fe13b7898e38dcd3b29ab82aeb3426634af567c9
Jonathan Hunter <hunterjonathant@gmail.com>
2016-04-11 02:15:22 -0400

    Random material picker

---
+++ Assets/Scripts/Boxing/RandomMaterialPicker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Boxing
+{
+    class RandomMaterialPicker : MonoBehaviour
+    {
+        public Material[] bodyMats;
+        public Material[] clotheMats;
+        public Material[] gloveMats;
+        public Material[] bootMats;
+
+        public GameObject[] bodys;
+        public GameObject[] clothes;
+        public GameObject[] gloves;
+        public GameObject[] boots;
+
+        void Start()
+        {
+            setToRand(bodys, bodyMats);
+            setToRand(clothes, clotheMats);
+            setToRand(gloves, gloveMats);
+            setToRand(boots, bootMats);
+        }
+
+        private void setToRand(GameObject[] part, Material[] mats)
+        {
+            int rand = Random.Range(0, mats.Length - 1);
+            foreach (GameObject g in part)
+                g.GetComponent<Renderer>().material = mats[rand];
+        }
+    }
+}
